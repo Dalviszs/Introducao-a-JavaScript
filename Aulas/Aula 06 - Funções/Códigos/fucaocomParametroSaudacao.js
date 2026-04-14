bd2a8f02adf121deb45d01279e3a829b7f3eabac
Dalvana Lopes Ribeiro <93749040+DalvanaRibeiro@users.noreply.github.com>
2026-04-14 16:04:38 -0300

    Create fucaocomParametroSaudacao.js

---
+++ Aulas/Aula 06 - Funções/Códigos/fucaocomParametroSaudacao.js
@@ -0,0 +1,7 @@
+function saudar(nome) {
+    return "Olá, " + nome + "! Bem-vindo(a)!"; // Concatena a saudação
+}
+
+// Testando
+console.log(saudar("Ana"));   // Saída: Olá, Ana! Bem-vindo(a)!
+console.log(saudar("Carlos")); // Saída: Olá, Carlos! Bem-vindo(a)!
